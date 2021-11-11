a3d43130b341727dd613eed5af36853956e5f038
erli1 <85357139+erli1@users.noreply.github.com>
2021-11-11 15:53:53 -0500

    Add files via upload
    Lattice example similar to "Simple.py"

---
+++ lattice.py
@@ -0,0 +1,24 @@
+from gpe import LatticeGPE
+
+#Load GPE
+sim = LatticeGPE("Lattice",2,0,80)
+
+# Grid parameters
+sim.setGridSize(32)
+
+# Contact interaction
+# Only the product (atomNumber - 1) * scatteringLength is important
+sim.set("atomNumber",       2)
+sim.set("scatteringLength", 1)
+
+# Dipolar interaction (switched off)
+sim.set("dipolarLength", 1)
+sim.set("evolutionDipolar", True)
+
+# Number of threads. Equals number of cores if not set
+# sim.setThreads(4)
+
+sim.initialize()
+
+# Imaginary time evolution
+sim.ite(steps=10000, monitorSteps=500, plotSteps=500)
