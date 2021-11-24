344147542b7c5aff0e46c09c55cc50ac85a8265f
erli1 <85357139+erli1@users.noreply.github.com>
2021-11-24 11:51:52 -0500

    Update SimulationParameters.h

--- gpe/SimulationParameters.h
+++ gpe/SimulationParameters.h
@@ -109,7 +109,7 @@ class SimulationParameters {
  
     double getLHYPreFactor() const {
         // factor = 32 * g * sqrt(a^3) / 3 / sqrt(pi) * (1 + 3/2 * edd^2) * sqrt((N - 1)^3)
-        return getContactInteractionFactor() * 32 / 3 * pow(sqrt(getDouble("scatteringLength")),3) / M_PI * (getDouble("atomNumber") - 1)) * 1 + (3 / 2) * (getDouble("dipolarLength") / pow(getDouble("scatteringLength")),2);
+        return getContactInteractionFactor() * 32 / 3 * sqrt(pow(getDouble("scatteringLength"),3) / M_PI) * (getDouble("atomNumber") - 1) * (1 + (3 / 2) * pow(getDouble("dipolarLength") / getDouble("scatteringLength"),2));
     }
 
     double getDipolarInteractionFactor() const {
