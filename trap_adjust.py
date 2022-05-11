0f8b9d9d8b31a8e28d5cd13943725954e45480b0
erli1 <85357139+erli1@users.noreply.github.com>
2022-05-11 13:25:35 -0400

    Update trap_adjust.py

--- trap_adjust.py
+++ trap_adjust.py
@@ -1,18 +1,18 @@
 from gpe import HarmonicTrapGPE
 
 # Load GPE
-sim = HarmonicTrapGPE("Real", 200.0, 10.0, 200.0)
+sim = HarmonicTrapGPE("Real", 190.0, 15.0, 191.0)
 
 # Grid parameters
-sim.setGridSize(128)
+sim.setGridSize(64)
 
 # Contact interaction
 # Only the product (atomNumber - 1) * scatteringLength is important
-sim.set("atomNumber", 10**6)
-sim.set("scatteringLength", 100.0)
+sim.set("atomNumber", 5*10**5)
+sim.set("scatteringLength", 85.0)
 
 # Dipolar interaction (switched on)
-sim.set("dipolarLength", 80.0)
+sim.set("dipolarLength", 75.0)
 sim.set("evolutionDipolar", True)
 
 # Number of threads. Equals number of cores if not set
