517d0b1d4eb8281fc01a58c9d7aef611d603fca0
erli1 <85357139+erli1@users.noreply.github.com>
2022-05-11 13:09:09 -0400

    Update trap_adjust.py

--- trap_adjust.py
+++ trap_adjust.py
@@ -1,7 +1,7 @@
 from gpe import HarmonicTrapGPE
 
 # Load GPE
-sim = HarmonicTrapGPE("Real", 156, 17.2, 198)
+sim = HarmonicTrapGPE("Real", 200, 10, 200)
 
 # Grid parameters
 sim.setGridSize(64)
@@ -9,10 +9,10 @@ sim.setGridSize(64)
 # Contact interaction
 # Only the product (atomNumber - 1) * scatteringLength is important
 sim.set("atomNumber",       10**5)
-sim.set("scatteringLength", 70)
+sim.set("scatteringLength", 100)
 
 # Dipolar interaction (switched on)
-sim.set("dipolarLength", 100)
+sim.set("dipolarLength", 80)
 sim.set("evolutionDipolar", True)
 
 # Number of threads. Equals number of cores if not set
@@ -21,13 +21,14 @@ sim.set("evolutionDipolar", True)
 sim.initialize()
 
 # Imaginary time evolution
-sim.ite(steps=1000, monitorSteps=500, plotSteps=500)
+sim.ite(steps=2000, monitorSteps=500, plotSteps=500)
 
 # Real time evolution
-sim.rte(steps=2000, monitorSteps=500, plotSteps=500)
+sim.rte(steps=4000, monitorSteps=500, plotSteps=500)
 
 # adjust trap
-sim.set("omegaY",21)
+sim.set("omegaY",20)
+sim.set("scatteringLength", 60)
 
 #resume evolution
-sim.rte(steps=2000, monitorSteps=500, plotSteps=500)
+sim.rte(steps=4000, monitorSteps=500, plotSteps=500)
