bed8c8346e7a0377e4055dae46f03e6beabe8aa6
erli1 <85357139+erli1@users.noreply.github.com>
2022-05-11 13:14:44 -0400

    Update trap_adjust.py

--- trap_adjust.py
+++ trap_adjust.py
@@ -1,18 +1,18 @@
 from gpe import HarmonicTrapGPE
 
 # Load GPE
-sim = HarmonicTrapGPE("Real", 200, 10, 200)
+sim = HarmonicTrapGPE("Real", 200.0, 10.0, 200.0)
 
 # Grid parameters
-sim.setGridSize(64)
+sim.setGridSize(128)
 
 # Contact interaction
 # Only the product (atomNumber - 1) * scatteringLength is important
-sim.set("atomNumber",       10**5)
-sim.set("scatteringLength", 100)
+sim.set("atomNumber", 10**6)
+sim.set("scatteringLength", 100.0)
 
 # Dipolar interaction (switched on)
-sim.set("dipolarLength", 80)
+sim.set("dipolarLength", 80.0)
 sim.set("evolutionDipolar", True)
 
 # Number of threads. Equals number of cores if not set
@@ -26,9 +26,9 @@ sim.ite(steps=2000, monitorSteps=500, plotSteps=500)
 # Real time evolution
 sim.rte(steps=4000, monitorSteps=500, plotSteps=500)
 
-# adjust trap
-sim.set("omegaY",20)
-sim.set("scatteringLength", 60)
+# adjust trap/quench
+sim.set("omegaY",20.0)
+sim.set("scatteringLength", 60.0)
 
 #resume evolution
 sim.rte(steps=4000, monitorSteps=500, plotSteps=500)
