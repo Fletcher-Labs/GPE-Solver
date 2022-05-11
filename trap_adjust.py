e2c9e73f5668da94ac409a27e37d6a249b6434dd
erli1 <85357139+erli1@users.noreply.github.com>
2022-05-11 19:57:09 -0400

    Update trap_adjust.py
    Testing "Droplet Crystal Ground States of a Dipolar Bose Gas"

--- trap_adjust.py
+++ trap_adjust.py
@@ -1,15 +1,15 @@
 from gpe import HarmonicTrapGPE
 
 # Load GPE
-sim = HarmonicTrapGPE("Real", 190.0, 15.0, 191.0)
+sim = HarmonicTrapGPE("Real", 60, 300, 60)
 
 # Grid parameters
-sim.setGridSize(128)
+sim.setGridSize(64)
 
 # Contact interaction
 # Only the product (atomNumber - 1) * scatteringLength is important
 sim.set("atomNumber", 2)
-sim.set("scatteringLength", 12)
+sim.set("scatteringLength", 8)
 
 # Dipolar interaction (switched on)
 sim.set("dipolarLength", 10)
@@ -21,14 +21,14 @@ sim.set("evolutionDipolar", True)
 sim.initialize()
 
 # Imaginary time evolution
-sim.ite(steps=5000, monitorSteps=1000, plotSteps=1000)
+sim.ite(steps=1500, monitorSteps=500, plotSteps=500)
 
 # Real time evolution
 sim.rte(steps=10000, monitorSteps=1000, plotSteps=1000)
 
 # adjust trap/quench
-sim.set("omegaY",20.0)
-sim.set("scatteringLength", 8.0)
+#sim.set("omegaY",20.0)
+#sim.set("scatteringLength", 8.0)
 
 #resume evolution
-sim.rte(steps=10000, monitorSteps=1000, plotSteps=1000)
+#sim.rte(steps=10000, monitorSteps=1000, plotSteps=1000)
