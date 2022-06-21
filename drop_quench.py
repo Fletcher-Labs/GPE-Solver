64f931bc3420ad64b71c3142d21df3c131bedde9
erli1 <85357139+erli1@users.noreply.github.com>
2022-06-21 14:34:15 -0400

    Update drop_quench.py

--- drop_quench.py
+++ drop_quench.py
@@ -20,15 +20,26 @@ sim.set("evolutionDipolar", True)
 
 sim.initialize()
 
-# Imaginary time evolution
+# initial imaginary time evolution
 sim.ite(steps=1000, monitorSteps=500, plotSteps=500)
 
 # Real time evolution
-sim.rte(steps=5000, monitorSteps=1000, plotSteps=1000)
+sim.rte(steps=2000, monitorSteps=1000, plotSteps=1000)
 
-# adjust trap/quench
-#sim.set("omegaY",20.0)
+
+# ROUGH QUENCH RAMP (small adjustment, equilibration, short evolution)
+sim.set("scatteringLength", 10.0)
+sim.ite(steps=500, monitorSteps=500, plotSteps=500)
+sim.rte(steps=1000, monitorSteps=1000, plotSteps=1000)
+sim.set("scatteringLength", 9.0)
+sim.ite(steps=500, monitorSteps=500, plotSteps=500)
+sim.rte(steps=1000, monitorSteps=1000, plotSteps=1000)
+sim.set("scatteringLength", 8.0)
+sim.ite(steps=500, monitorSteps=500, plotSteps=500)
+sim.rte(steps=1000, monitorSteps=1000, plotSteps=1000)
 sim.set("scatteringLength", 7.0)
+sim.ite(steps=500, monitorSteps=500, plotSteps=500)
+sim.rte(steps=1000, monitorSteps=1000, plotSteps=1000)
 
-#resume evolution
-sim.rte(steps=6000, monitorSteps=1000, plotSteps=1000)
+#resume evolution to see droplet formation
+sim.rte(steps=5000, monitorSteps=1000, plotSteps=1000)
