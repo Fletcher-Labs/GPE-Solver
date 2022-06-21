337996936e21db4bc7dafae96f4c6083b116fe90
erli1 <85357139+erli1@users.noreply.github.com>
2022-06-21 15:23:34 -0400

    Update drop_quench.py

--- drop_quench.py
+++ drop_quench.py
@@ -21,25 +21,25 @@ sim.set("evolutionDipolar", True)
 sim.initialize()
 
 # initial imaginary time evolution
-sim.ite(steps=1000, monitorSteps=500, plotSteps=500)
+sim.ite(steps=3000, monitorSteps=500, plotSteps=500)
 
 # Real time evolution
-sim.rte(steps=2000, monitorSteps=1000, plotSteps=1000)
+sim.rte(steps=4000, monitorSteps=1000, plotSteps=1000)
 
 
 # ROUGH QUENCH RAMP (small adjustment, equilibration, short evolution)
 sim.set("scatteringLength", 10.0)
-sim.ite(steps=500, monitorSteps=500, plotSteps=500)
-sim.rte(steps=1000, monitorSteps=1000, plotSteps=1000)
+sim.ite(steps=50, monitorSteps=50, plotSteps=50)
+sim.rte(steps=100, monitorSteps=100, plotSteps=100)
 sim.set("scatteringLength", 9.0)
-sim.ite(steps=500, monitorSteps=500, plotSteps=500)
-sim.rte(steps=1000, monitorSteps=1000, plotSteps=1000)
+sim.ite(steps=50, monitorSteps=50, plotSteps=50)
+sim.rte(steps=100, monitorSteps=100, plotSteps=100)
 sim.set("scatteringLength", 8.0)
-sim.ite(steps=500, monitorSteps=500, plotSteps=500)
-sim.rte(steps=1000, monitorSteps=1000, plotSteps=1000)
+sim.ite(steps=50, monitorSteps=50, plotSteps=50)
+sim.rte(steps=100, monitorSteps=100, plotSteps=100)
 sim.set("scatteringLength", 7.0)
-sim.ite(steps=500, monitorSteps=500, plotSteps=500)
-sim.rte(steps=1000, monitorSteps=1000, plotSteps=1000)
+sim.ite(steps=50, monitorSteps=50, plotSteps=50)
+sim.rte(steps=100, monitorSteps=100, plotSteps=100)
 
 #resume evolution to see droplet formation
 sim.rte(steps=5000, monitorSteps=1000, plotSteps=1000)
