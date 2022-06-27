59e57301382a125ab30eb63d9ba77ccce02515a8
erli1 <85357139+erli1@users.noreply.github.com>
2022-06-27 11:43:37 -0400

    Update drop_quench.py

--- drop_quench.py
+++ drop_quench.py
@@ -1,18 +1,18 @@
 from gpe import HarmonicTrapGPE
 
 # Load GPE
-sim = HarmonicTrapGPE("Real", 200, 200, 75)
+sim = HarmonicTrapGPE("Real", 280, 280, 850)
 
 # Grid parameters
 sim.setGridSize(64)
 
 # Contact interaction
 # Only the product (atomNumber - 1) * scatteringLength is important
-sim.set("atomNumber", 2)
-sim.set("scatteringLength", 11.0)
+sim.set("atomNumber", 15000)
+sim.set("scatteringLength", 110.0)
 
 # Dipolar interaction (switched on)
-sim.set("dipolarLength", 9.0)
+sim.set("dipolarLength", 132.0)
 sim.set("evolutionDipolar", True)
 
 # Number of threads. Equals number of cores if not set
@@ -24,25 +24,20 @@ sim.initialize()
 sim.ite(steps=3000, monitorSteps=500, plotSteps=500)
 
 # Real time evolution
-sim.rte(steps=4000, monitorSteps=1000, plotSteps=1000)
+#sim.rte(steps=4000, monitorSteps=1000, plotSteps=1000)
 
 
 # ROUGH QUENCH RAMP (small adjustment, equilibration, short evolution)
-sim.set("scatteringLength", 10.0)
-#sim.ite(steps=50, monitorSteps=50, plotSteps=50)
-sim.rte(steps=100, monitorSteps=100, plotSteps=100)
-sim.set("scatteringLength", 9.0)
-#sim.ite(steps=50, monitorSteps=50, plotSteps=50)
-sim.rte(steps=100, monitorSteps=100, plotSteps=100)
-sim.set("scatteringLength", 8.0)
-#sim.ite(steps=50, monitorSteps=50, plotSteps=50)
-sim.rte(steps=100, monitorSteps=100, plotSteps=100)
-sim.set("scatteringLength", 7.0)
-#sim.ite(steps=50, monitorSteps=50, plotSteps=50)
+#sim.set("scatteringLength", 10.0)
 #sim.rte(steps=100, monitorSteps=100, plotSteps=100)
+#sim.set("scatteringLength", 9.0)
+#sim.rte(steps=100, monitorSteps=100, plotSteps=100)
+#sim.set("scatteringLength", 8.0)
+#sim.rte(steps=100, monitorSteps=100, plotSteps=100)
+#sim.set("scatteringLength", 7.0)
 
 # resume evolution to see droplet formation
-sim.rte(steps=12700, monitorSteps=1000, plotSteps=1000)
+#sim.rte(steps=12700, monitorSteps=1000, plotSteps=1000)
 
 # imaginary time evolution to find droplet ground state
 sim.ite(steps=3000, monitorSteps=500, plotSteps=500)
